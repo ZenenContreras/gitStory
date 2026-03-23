65f4f6c4511f468c444679b93b9cb7de6c7d2198
Zenen Contreras <zenencontreras1@gmail.com>
2026-03-23 12:08:21 -0500

    fix: animation with GSAP: timeline and scroll trigger

--- src/components/landing/GrowSection.tsx
+++ src/components/landing/GrowSection.tsx
@@ -1,7 +1,13 @@
 import { type JSX } from "react"
 import { GitBranch, PartyPopper,FolderGit2 } from "lucide-react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger) 
 
 interface steps{
+    name: string;
     order: number;
     title: string;
     text: string;
@@ -10,20 +16,38 @@ interface steps{
 
 
 const steps: steps[] = [
-    {order: 1 , title: 'Connect Your Github', text: 'Securly link yout repositories with one click OAuth integration', icon: <FolderGit2 size={36}/> },
-    {order: 2 , title: 'Pick a PR or Commit', text: 'Select any recent pull request or commit. Our AI analyses the code changes and context', icon: <GitBranch size={36}/> },
-    {order: 3 , title: 'Copy, Paste, and get hired', text: 'Get a high-engagement post ready to share with your network.', icon: <PartyPopper size={36}/>}
+
+    {name: 'connect' ,order: 1 , title: 'Connect Your Github', text: 'Securly link yout repositories with one click OAuth integration', icon: <FolderGit2 size={36}/> },
+
+    {name: 'pick' ,order: 2 , title: 'Pick a PR or Commit', text: 'Select any recent pull request or commit. Our AI analyses the code changes and context', icon: <GitBranch size={36}/> },
+
+    {name: 'hired' , order: 3 , title: 'Copy, Paste, and get hired', text: 'Get a high-engagement post ready to share with your network.', icon: <PartyPopper size={36}/>}
 ]
 
 
 function GrowSection() {
+    useGSAP(() => {
+        let tl = gsap.timeline({
+            scrollTrigger:{
+                trigger: '#steps',
+                start: 'top bottom',
+                end: 'top 40%',
+                scrub: 1,
+            }
+        })
+
+        tl.from('#connect', {opacity: 0, y: 50 })
+          .from('#pick', {opacity: 0, y: 50 })
+          .from('#hired', {opacity: 0, y: 50 })
+    }, [])
+
   return (
     <section className="flex flex-col items-center py-16 md:py-24">
         <h2 className="font-black  dark:text-slate-100 text-3xl md:text-4xl mb-8">How It Works</h2>
         <p className="font-mono dark:text-secondary text-center text-sm md:text-md mb-24">Transform your development workflow into a social growth engine in <span className="font-bold">three</span> simple steps</p>
-        <div className="flex flex-col md:flex-row  gap-18 md:gap-4">
+        <div id='steps' className="flex flex-col md:flex-row  gap-18 md:gap-4">
             {steps.map((step)=> (
-                <div key={step.order} className="flex flex-col items-center gap-5">
+                <div id={step.name} key={step.order} className="flex flex-col items-center gap-5">
                     <div className="p-2 text-primary rounded-xl shadow shadow-primary bg-primary/20">
                         {step.icon}
                     </div>
